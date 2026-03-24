e4cc6f4275b4c1c297b4f342335740431dd3abff
langamanjb <langamanjb789@mail.sandhills.edu>
2026-03-24 14:03:21 -0400

    Weapon Detection App
    This is the start of something great!

---
+++ SwiftProject/WeaponDetectionApp/WeaponDetectionApp/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  WeaponDetectionApp
+//
+//  Created by Joshua Langaman and Lane Evans on 3/24/26.
+//
+
+import SwiftUI
+
+struct ContentView: View
+{
+    var body: some View
+    {
+        VStack(spacing: 10)
+        {
+            
+        }
+        
+    }
+}
+
+#Preview
+    {
+    ContentView()
+}
---
+++ SwiftProject/WeaponDetectionApp/WeaponDetectionApp/WeaponDetectionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeaponDetectionAppApp.swift
+//  WeaponDetectionApp
+//
+//  Created by Joshua Langaman on 3/24/26.
+//
+
+import SwiftUI
+
+@main
+struct WeaponDetectionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
